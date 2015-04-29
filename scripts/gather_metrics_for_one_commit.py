e6e7c504ac7bdd60a197c81e3067940bdc7acb49
Jeremy Boyd <boydjj@gmail.com>
2015-04-28 23:23:51 -0500

    Add one-off script I used to get Django commit data

---
+++ scripts/gather_metrics_for_one_commit.py
@@ -0,0 +1,12 @@
+import os
+
+from gather_metrics import DATA_ROOT_DIR, PROJECT_ROOT_DIR
+import run_metrics
+
+import django_project as project
+
+data_dir = os.path.join(DATA_ROOT_DIR, project.DIR)
+project_dir = os.path.join(PROJECT_ROOT_DIR, project.DIR)
+commit = '941e11e54d175f4ffc769852e5dd376ec1ba5239'
+
+run_metrics.gather_metrics(commit, project_dir, data_dir)
